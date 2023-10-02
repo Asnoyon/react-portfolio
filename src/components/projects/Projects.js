8d1f70c91aec592508885b95404a48fe86c22631
Asnoyon <blackpain1100@gmail.com>
2023-10-03 00:29:15 +0600

    update project synchornization

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -16,28 +16,25 @@ const Projects = () => {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-      <ProjectsCard
-          title="Sign Up page"
-          des="Complete responsive registration page with beautiful design.Created using HTML,CSS. Here I learned how to use linear gradient"
-          src={projectOne}
-          githubLink="https://github.com/Asnoyon/signup"
-          websiteLink="https://signup-omega-nine.vercel.app/"
-        />
-        <ProjectsCard
-          title="Islam Jahid & Co."
-          des="This was a company website. Here I learned how to design figma to html.This was my first figma to html."
-          src={projectTwo}
-          githubLink="https://github.com/Asnoyon/figma_html"
-          websiteLink="https://figma-html-six.vercel.app/"
 
-        />
+
+
+
+
 
         <ProjectsCard
-          title="Noyon"
-          des="This is my first portfolio with bootstrap.Created using HTML,CSS and BOOTSTRAP.Here I learned Bootstrap card"
-          src={projectFour}
-          githubLink="https://github.com/Asnoyon/Envelope"
-          websiteLink="https://envelope-one.vercel.app/"
+          title="E-commerce"
+          des="This is E-commerce app. Created HTML,CSS and REACT js. Here I learned lot of things like context API, Tailwind css and React router dom "
+          src={projectSix}
+          githubLink="https://github.com/Asnoyon/e-commerce"
+          websiteLink="https://e-commerce-tau-murex-42.vercel.app/"
+        />
+        <ProjectsCard
+          title="Quote Generator"
+          des="This is my Quote Genrator app.Created react jsx and css.Here I learned how fetch data with api"
+          src={projectThree}
+          githubLink="https://github.com/Asnoyon/react-quote-gen"
+          websiteLink="https://react-quote-gen.vercel.app/"
         />
         <ProjectsCard
           title="ToDo"
@@ -46,19 +43,26 @@ const Projects = () => {
           githubLink="https://github.com/Asnoyon/react-todo"
           websiteLink="https://react-todo-delta-lemon.vercel.app/"
         />
-          <ProjectsCard
-          title="Quote Generator"
-          des="This is my Quote Genrator app.Created react jsx and css.Here I learned how fetch data with api"
-          src={projectThree}
-          githubLink="https://github.com/Asnoyon/react-quote-gen"
-          websiteLink="https://react-quote-gen.vercel.app/"
+        <ProjectsCard
+          title="Noyon"
+          des="This is my first portfolio with bootstrap.Created using HTML,CSS and BOOTSTRAP.Here I learned Bootstrap card"
+          src={projectFour}
+          githubLink="https://github.com/Asnoyon/Envelope"
+          websiteLink="https://envelope-one.vercel.app/"
         />
         <ProjectsCard
-          title="E-commerce"
-          des="This is E-commerce app. Created HTML,CSS and REACT js. Here I learned lot of things like context API, Tailwind css and React router dom "
-          src={projectSix}
-          githubLink="https://github.com/Asnoyon/e-commerce"
-          websiteLink="https://e-commerce-tau-murex-42.vercel.app/"
+          title="Islam Jahid & Co."
+          des="This was a company website. Here I learned how to design figma to html.This was my first figma to html."
+          src={projectTwo}
+          githubLink="https://github.com/Asnoyon/figma_html"
+          websiteLink="https://figma-html-six.vercel.app/" 
+        />
+        <ProjectsCard
+          title="Sign Up page"
+          des="Complete responsive registration page with beautiful design.Created using HTML,CSS. Here I learned how to use linear gradient"
+          src={projectOne}
+          githubLink="https://github.com/Asnoyon/signup"
+          websiteLink="https://signup-omega-nine.vercel.app/"
         />
       </div>
     </section>
