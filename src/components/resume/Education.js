402b705753a46eb3d873a2242f7a601027a035b8
Asnoyon <blackpain1100@gmail.com>
2023-10-14 22:36:42 +0600

    educational year remove

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -12,7 +12,7 @@ const Education = () => {
       {/* part one */}
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-[#00FA9A] tracking-[4px]">2012-2021</p>
+          <p className="text-sm text-[#00FA9A] tracking-[4px]"></p>
           <h2 className="text-3xl md:text-4xl font-bold">Educational Qualification</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
